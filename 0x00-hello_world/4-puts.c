86e69d9a9b0f2113133db72431cecb7cac5a88f2
Joseph-Akin <jspacle867@gmail.com>
2022-09-08 17:06:57 -0700

    Akin

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
-#!/bin/bash
 #include <studio.h>
 
 /**
- *
- * * Description: main - Print out a string to stdout.
- *
- * * Return: 0 if success.
- *
- * */
+ * Description: main - Print out a string to stdout.
+ * Return: 0 if success.
+ */
 
 int main(void)
 
@@ -17,4 +13,4 @@ int main(void)
 
 	return (0);
 
-}
+} 
