a9dff14cf8cbdc03e372328f4a3a93759adfff91
Joseph-Akin <jspacle867@gmail.com>
2022-09-09 03:10:54 -0700

    Akin

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - main block 
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1)
+}
