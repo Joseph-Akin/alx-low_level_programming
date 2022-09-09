2deaed88fdf5bb9fb3935ba8df0d185379809b94
Joseph-Akin <jspacle867@gmail.com>
2022-09-08 21:58:08 -0700

    Akin

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,25 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-/**
- * main - print different various types 
- *
- * Return: Always 0 (Success)  
- */
+/***
+* main - print different various types 
+*
+* Description: using the main function
+* this program prints "programming is like building a multilingual puzzle 
+* Return: Always 0 (Success) 
+*/
 int main(void)
 {
-        int a;
-        long long int b; 
-        char c;
-        float d;
-        long int e;
+        char c; 
+        int i; 
+        long li; 
+        long long lli; 
+        float f;   
 
-        printf("size of a char: %d byte(s)\n", sizeof(char)); 
-        printf("size of an         printf("size of a long int: %d byte(s)\n", sizeof(long int)); 
-        printf("size of a long long int: %d byte(s)\n", sizeof(long long int));  
+        printf("size of a char: %d byte(s)\n", sizeof(c));     
+        printf("size of an int: %d byte(s)\n", sizeof(i)); 
+        printf("size of a long int: %d byte(s)\n", sizeof(li)); 
+        printf("size of a long long int: %d byte(s)\n", sizeof(lli)); 
         printf("size of a long int: %d byte(s)\n", sizeof(long int)); 
-        printf("size of a float: %d byte(s)\n", sizeof(float));
+        printf("size of a float: %d byte(s)\n", sizeof(f));
         return (0);
 }
