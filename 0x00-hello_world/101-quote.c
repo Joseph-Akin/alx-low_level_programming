7a63e3c7333f19a5da653489c6cec2a2e48f18f3
Joseph-Akin <jspacle867@gmail.com>
2022-09-09 03:30:37 -0700

    Akin

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - entry point
- *
- * Description: prints and that piece of art is useful 
- * Return: 1 if sucessfull
- */
+* main - entry point
+*
+* Description: prints and that piece of art is useful 
+* Return: 1 if sucessfull
+*/
 int main(void)
 {
-	write(2, "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
