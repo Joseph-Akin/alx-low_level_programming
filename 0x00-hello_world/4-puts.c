d32f9c2b0f8aa2dcade69a54a71c16fde77714fe
Joseph-Akin <jspacle867@gmail.com>
2022-09-08 18:13:17 -0700

    Akin

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * Description: main - Print out a string to stdout.
  * Return: 0 if success.
@@ -7,4 +7,4 @@ int main(void)
 {
 	puts("\"  Programming is like building a multilingual puzzle");
 	return (0);
-}
+} 
