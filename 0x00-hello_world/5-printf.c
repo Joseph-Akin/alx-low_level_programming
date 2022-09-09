4745ec3fea3e8674f54e001001fede68fb78a27f
Joseph-Akin <jspacle867@gmail.com>
2022-09-08 18:49:50 -0700

    Akin

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* Description: main - Print out a string to stdout.
+* Return: 0 if success.
+*/
+int main(void)
+{
+	puts("\"with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
