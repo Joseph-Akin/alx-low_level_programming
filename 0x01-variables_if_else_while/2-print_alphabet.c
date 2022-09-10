661d8df59a7c2f0bffad02360500db51d311a1d5
Joseph-Akin <jspacle867@gmail.com>
2022-09-09 19:01:10 -0700

    Akin

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints the alphabets in lower case \n
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z')
+	{
+		putchar(ch)
+	}
+	putchar('\n');
+return (0);
+}
