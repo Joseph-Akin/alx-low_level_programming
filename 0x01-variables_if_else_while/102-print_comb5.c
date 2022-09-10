5efa30e871a77b125aeb4aa26d03e32be8a25056
Joseph-Akin <jspacle867@gmail.com>
2022-09-10 08:21:20 -0700

    Akin

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,28 +16,28 @@ int main(void)
 			{
 				for (j = 48; j <= 57; j++)
 				{
-					if (((k + j) > (c + i) && k >= c) || c <k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
+				if (((k + j) > (c + i) && k >= c) || c <k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
 
-						if (c + i + k + j == 227 && c == 57)
-						{
-						break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
 					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
 					}
+				}
 				}
 			}
 		}
+	}
 	putchar('\n');
 	return (0);
 }
