cfc660eee4abc53839f940f90767c2c5b9507012
Joseph-Akin <jspacle867@gmail.com>
2022-09-13 12:53:26 -0700

    Akin

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet -Entry point
+ * description: a function that prints the alphabets,
+ * in lowecase followed by a new line 
+ * Return void
+ */
+void print_alphabet(void)
+{
+	chat letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putcha(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+}
