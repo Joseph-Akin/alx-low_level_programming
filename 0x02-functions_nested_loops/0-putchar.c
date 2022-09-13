8c10cf288234386bc122f65b3d208efbce958ed4
Joseph-Akin <jspacle867@gmail.com>
2022-09-13 11:41:22 -0700

    Akin

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,21 @@
-#!/bin/bash
-printf(-putchar \n)
+#include "stdio.h"
+#include "main.h"
+/**
+ * main - entry point
+ * description: prints '_putchat\n'
+ * retutn: always 0 (success)
+ */
+int main(void)
+{
+char str[] = "_putchar\n";
+int 1 = 0;
+
+while (str[i] != '\0')
+	{
+	char c = str[i];
+
+	_putchar(c)
+	i++;
+	}
+return (0);
+}
