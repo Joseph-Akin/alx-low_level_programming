ddc7bb76dddae491f8a4eee1953024baa7d77fd7
Joseph-Akin <jspacle867@gmail.com>
2022-09-13 17:10:19 -0700

    akin

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet 10 times the alphabet, in lowercase, followed by a new line
- * return void 0 
+ * print_alphabet_10 times - make the alphabet x10 times more
+ * return void 
  */
 
 void print_alphabet_x10(void)
