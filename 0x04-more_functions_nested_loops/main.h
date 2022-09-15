888c943b09bdab2ae9016ac11d94f7b32987ee0b
Joseph-Akin <jspacle867@gmail.com>
2022-09-15 06:43:47 -0700

    akin

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,4 +9,3 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
