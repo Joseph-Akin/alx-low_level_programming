808e30299d2ff48fac079f1e5a9d7ee7f546cd5e
Joseph-Akin <jspacle867@gmail.com>
2022-09-19 07:54:15 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,12 +4,6 @@
  * swap_int - swaps the values of a and b
  * @a: pointer 1
  * @b: pointer 2
-#include "main.h"
-
-/**
- * swap_int - swaps the values of a and b
- * @a: pointer 1
- * @b: pointer 2
  * Return; 0
  */
 
